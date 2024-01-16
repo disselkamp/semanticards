d6cbcce3d3a128c6db938ff6ff9c8d042dd3e34b
constantin <27238892+disselkamp@users.noreply.github.com>
2024-01-16 12:45:49 +0100

    add shuffle and refactor

--- recall.py
+++ recall.py
@@ -5,15 +5,16 @@ from sentence_transformers import SentenceTransformer
 from numpy import dot
 from numpy.linalg import norm
 
-MODEL = SentenceTransformer("all-MiniLM-L6-v2")
+MODEL = SentenceTransformer("all-MiniLM-L6-v2")  # 120 mb
+MIN_SIMILARITY = 0.9
 
 
 @dataclass
 class Flashcard:
     question: str = ""
     answer: str = ""
-    correct_answers: list[str] = field(default_factory=list)
-    wrong_answers: list[str] = field(default_factory=list)
+    correct_user_answers: list[str] = field(default_factory=list)
+    wrong_user_answers: list[str] = field(default_factory=list)
 
 
 def add_deck_to_flashcards(deck, flashcards):
@@ -39,38 +40,36 @@ def add_deck_to_flashcards(deck, flashcards):
 
 
 def is_correct(user_answer, flashcard):
-    user_answer, original_answer = MODEL.encode([user_answer, flashcard.answer])
-    correct_answers = MODEL.encode(flashcard.correct_answers)
-    wrong_answers = MODEL.encode(flashcard.wrong_answers)
+    user_answer = MODEL.encode(user_answer)
+    correct_answers = MODEL.encode(
+        [flashcard.answer] + flashcard.correct_user_answers
+    )
+    wrong_answers = MODEL.encode(flashcard.wrong_user_answers)
 
     def cos_sim(x, y):
         return dot(x, y) / (norm(x) * norm(y))
 
-    sim_orig_ans = cos_sim(user_answer, original_answer)
     sim_correct_anss = [cos_sim(user_answer, a) for a in correct_answers]
     sim_wrong_anss = [cos_sim(user_answer, a) for a in wrong_answers]
 
-    sim_correct = (
-        sim_orig_ans
-        if len(sim_correct_anss) == 0
-        else max(sim_orig_ans, *sim_correct_anss)
-    )
-    sim_wrong = 0 if len(sim_wrong_anss) == 0 else max(sim_wrong_anss)
+    sim_nearest_correct = max(sim_correct_anss)
+    sim_nearest_wrong = 0 if len(sim_wrong_anss) == 0 else max(sim_wrong_anss)
 
-    if sim_wrong > sim_correct:
+    if sim_nearest_wrong > sim_nearest_correct:
         return False
-    elif sim_correct > 0.9:
+    elif sim_nearest_correct > MIN_SIMILARITY:
         return True
     else:
         return False
 
 
-def main(decks):
+def main(decks, shuffle):
     flashcards = []
     for deck in decks:
         add_deck_to_flashcards(deck, flashcards)
 
-    # random.shuffle(flashcards)
+    if shuffle:
+        random.shuffle(flashcards)
 
     while True:
         flashcard = random.choice(flashcards)
@@ -85,9 +84,9 @@ def main(decks):
             print("Please type Y or n")
         if user_input in ["N", "n"]:
             if correct:
-                flashcard.wrong_answers.append(user_answer)
+                flashcard.wrong_user_answers.append(user_answer)
             else:
-                flashcard.correct_answers.append(user_answer)
+                flashcard.correct_user_answers.append(user_answer)
 
 
 if __name__ == "__main__":
@@ -99,5 +98,10 @@ if __name__ == "__main__":
         nargs="+",
         help="the flashcards you want to recall",
     )
+    parser.add_argument(
+        "--shuffle",
+        action="store_true",
+        help="do you want to shuffle the decks?",
+    )
     args = parser.parse_args()
-    main(args.decks)
+    main(args.decks, args.shuffle)
